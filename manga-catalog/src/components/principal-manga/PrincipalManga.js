71146010055056d7df7c17c765544c74ad8c11ba
filipearray <codedforgetbro@gmail.com>
2023-03-22 01:17:15 -0300

    fourth

--- manga-catalog/src/components/principal-manga/PrincipalManga.js
+++ manga-catalog/src/components/principal-manga/PrincipalManga.js
@@ -12,24 +12,28 @@ export default ({ item }) => {
     >
       <div className="featured-vertical">
         <div className="featured-horizontal">
-          <div className="featured-name">{item.name}</div>
-
-          <div className="featured-info">
-            <div className="featured-points">★ {item.score}</div>
-            <div className="featured-year">{item.publishingYear}</div>
-          </div>
-          <div className="featured-description">{item.description}</div>
-          <div className="featured-buttons">
-            <a href="" className="featured-watchbutton">
-              ▶ Read
-            </a>
-            <a href="" className="featured-mylistbutton">
-              + Add to Favorites
-            </a>
-          </div>
-
-          <div className="featured-genres">
-            Genres: {item.genres.join(", ")}
+          <div className="featured-2-horizontal">
+          <div className="separator"></div>
+          <div className="separator2"></div>
+              <div className="featured-points">★ {item.score}</div>
+              <div className="featured-year">{item.publishingYear}</div>  
+              <div className="featured-author">{item.story}</div> 
+              <div className="featured-demographic">{item.demographic}</div> 
+              <div className="featured-chapters">{item.chapters} chp.</div>
+              <div className="featured-vol">{item.volumes} vol.</div>
+              <div className="featured-members">{item.members} readers</div>
+              <div className="featured-serialization">{item.serialization}</div>
+              <div className="featured-genres">
+                  {item.genres.join(", ")}
+            </div>
+              <a href="" className="featured-watchbutton">
+                ▶ Read  
+              </a>
+              <a href="" className="featured-mylistbutton">
+                + Add to Favorites
+              </a>
+            <div className="featured-name">{item.name}</div> 
+            <div className="featured-description">{item.description}</div>
           </div>
         </div>
       </div>
